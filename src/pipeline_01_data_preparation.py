6f71b3ba3f80172b48ab34cd4af3e19d8206297b
Saurabh Maurya <saurabh08281997@gmail.com>
2022-08-24 09:17:13 +0530

    read config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -17,7 +17,7 @@ def main(config_path, datasource):
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    default_config_path = os.path.join("config", "params.yaml")
+    default_config_path = os.path.join("../config", "params.yaml")
     args.add_argument("--config", default=default_config_path)
     args.add_argument("--datasource", default=None)
 
